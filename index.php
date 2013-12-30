6a22f74396a99ab49c4439ce686a0d9fb2c00f08
Antonio Kobashikawa Carrasco <akobashikawa@gmail.com>
2013-12-30 02:10:44 -0500

    Using QueryPath

--- index.php
+++ index.php
@@ -2,17 +2,16 @@
 $p = isset($_GET['p']) ? $_GET['p'] : 'html/index.html';
 $q = isset($_GET['q']) ? $_GET['q'] : '';
 if (file_exists($p)) {
+	require_once 'QueryPath/src/qp.php';
+
 	$basehref = dirname($_SERVER['PHP_SELF']) . '/' . dirname($p) . '/';
-	$basehref = str_replace('html/', '', $basehref);
-	$html = file_get_contents($p);
-	$html = str_replace('<head>', '<head><base href="' . $basehref . '">', $html);
-	$html = process($html, $q);
-	echo $html;
+	$qp = htmlqp($p)
+		->find('head')
+		->prepend('<base href="' . $basehref . '"/>');
+
+	$qp->writeHTML();
+
 } else {
 	echo 'File not found: ' . $p;
 }
 echo '<pre>' . print_r($_GET, TRUE) . '</pre>';
-
-function process($html, $q) {
-	return $html;
-}
